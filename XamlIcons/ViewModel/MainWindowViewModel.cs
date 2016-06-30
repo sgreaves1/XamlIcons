600e9bf25a69621850edeabbe9a7b69001ab560e
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-06-30 09:39:19 +0100

    Added a list of icons to the view model

--- XamlIcons/ViewModel/MainWindowViewModel.cs
+++ XamlIcons/ViewModel/MainWindowViewModel.cs
@@ -1,6 +1,32 @@
-﻿namespace XamlIcons.ViewModel
+﻿using System.Collections.ObjectModel;
+using XamlIcons.Model;
+
+namespace XamlIcons.ViewModel
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : BaseViewModel
     {
+        private ObservableCollection<IconModel> _icons = new ObservableCollection<IconModel>();
+        private IconModel _selectedIcon;
+
+        public MainWindowViewModel()
+        {
+            
+        }
+
+        public ObservableCollection<IconModel> Icons
+        {
+            get { return _icons; }
+            set { _icons = value; }
+        }
+
+        public IconModel SelectedIcon
+        {
+            get { return _selectedIcon; }
+            set
+            {
+                _selectedIcon = value;
+                OnPropertyChanged();
+            }
+        }
     }
 }
