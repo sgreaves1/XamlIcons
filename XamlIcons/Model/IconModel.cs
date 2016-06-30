a96260b03f7c765cd2b7298b037633a90dc222ad
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-06-30 09:34:27 +0100

    Added an icon model class

---
+++ XamlIcons/Model/IconModel.cs
@@ -0,0 +1,28 @@
+﻿namespace XamlIcons.Model
+{
+    public class IconModel : BaseModel
+    {
+        private string _name;
+        private string _data;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        public string Data
+        {
+            get { return _data; }
+            set
+            {
+                _data = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
