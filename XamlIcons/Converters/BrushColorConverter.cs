a5819e21cbf683fe33bb985602b8b158c02d5e79
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-06-30 13:32:01 +0100

    made the fill check box work

---
+++ XamlIcons/Converters/BrushColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace XamlIcons.Converters
+{
+    public class BrushColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+            {
+                {
+                    return new SolidColorBrush(Colors.Black);
+                }
+            }
+            return new SolidColorBrush(Colors.Transparent);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
