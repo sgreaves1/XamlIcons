e4bd207013d6cb5e7b47d30d2b6fb3230839f891
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-07-12 11:16:08 +0100

    Place icon display information into a usercontrol

---
+++ XamlIcons/UserControl/IconInfoDisplay.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using XamlIcons.Model;
+
+namespace XamlIcons.UserControl
+{
+    /// <summary>
+    /// Interaction logic for IconInfoDisplay.xaml
+    /// </summary>
+    public partial class IconInfoDisplay 
+    {
+        /// <summary>
+        /// Dependency Property for the Icon 
+        /// </summary>
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon",
+                typeof(IconModel), 
+                typeof(IconInfoDisplay), 
+                new PropertyMetadata(null));
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public IconInfoDisplay()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// The icon this display will be based on
+        /// </summary>
+        public IconModel Icon
+        {
+            get { return (IconModel)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+    }
+}
